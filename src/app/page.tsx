853e03df03847a2129a35e6e73a88613ac6a6299
jimmygu <jimmygujh@gmail.com>
2025-07-18 10:59:53 +0800

    feat: add connect and disconnect button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,38 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+"use client";
+
+import { useConnectModal } from '@rainbow-me/rainbowkit';
+import { useAccount, useDisconnect } from 'wagmi';
 
 export default function Home() {
+  const modal = useConnectModal();
+  const { disconnect } = useDisconnect();
+  const { address, isConnected } = useAccount();
+
   return (
     <div className="w-screen h-screen">
       <div className="w-full flex justify-center pt-[100px]">
-        <ConnectButton />
+        {
+          isConnected ? (
+            <div className="flex flex-col items-center gap-[10px]">
+              <p>{address}</p>
+              <button
+                type="button"
+                onClick={() => disconnect()}
+                className="px-[30px] h-[40px] border border-black bg-[#FFDC50] text-black text-[16px] rounded-[4px]"
+              >
+                Disconnect
+              </button>
+            </div>
+          ) : (
+            <button
+              type="button"
+              onClick={() => modal?.openConnectModal?.()}
+              className="px-[30px] h-[40px] border border-black bg-[#FFDC50] text-black text-[16px] rounded-[4px]"
+            >
+              Connect Wallet
+            </button>
+          )
+        }
       </div>
     </div>
   );
